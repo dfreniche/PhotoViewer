30829ab3ba6d80faf36895e50a9256f2f0201829
Diego Freniche Brito <diego@freniche.com>
2015-11-23 22:31:53 +0100

    Initial commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Diego Freniche Brito on 23/11/15.
+//  Copyright © 2015 Diego Freniche Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
