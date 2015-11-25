6a577d9ca90b497292e81000d55ef07a1cea505a
Diego Freniche Brito <diego@freniche.com>
2015-11-25 23:23:54 +0100

    Sync, added PhotoLIst, added APIWrapper500px, added tests for APIWrapper500px

---
+++ PhotoViewer/Model/CollectionProtocol.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+protocol CollectionProtocol {
+    typealias T
+    
+    func count() -> Int
+    func add(element: T)
+    func elementAt(position: Int) -> T?
+    func getAll() -> [T]
+}
